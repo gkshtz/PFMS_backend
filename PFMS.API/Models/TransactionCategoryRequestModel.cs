6638bcdc2daa01549ba32111ec8e4e6b1876abc8
gkshtz <gkshitiz21@gmail.com>
2024-10-03 22:48:47 +0530

    Change in Categories schema

---
+++ PFMS.API/Models/TransactionCategoryRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using PFMS.DAL.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using PFMS.Utils.Enums;
+
+namespace PFMS.API.Models
+{
+    public class TransactionCategoryRequestModel
+    {
+        public string CategoryName { get; set; }
+
+        public TransactionType TransactionType { get; set; }
+    }
+}
