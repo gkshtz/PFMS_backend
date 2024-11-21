078180e9a518484bfebc01f4b1254a89a412d975
gkshtz <gkshitiz21@gmail.com>
2024-11-21 18:54:27 +0530

    User Controller tests

--- PFMS.API.Tests/Controllers/UserControllerTests.cs
+++ PFMS.API.Tests/Controllers/UserControllerTests.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.Xml;
-using System.Text;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Identity.Client;
 using Moq;
 using PFMS.API.Controllers;
 using PFMS.API.Models;
@@ -18,7 +11,7 @@ namespace PFMS.API.Tests.Controllers
     public class UserControllerTests
     {
         [Fact]
-        public async void Test()
+        public async void Gell_All_Users_When_Users_List_Is_Returned_Test()
         {
             // Arrange
             Mock<IUserService> userService = new Mock<IUserService>();
@@ -39,6 +32,52 @@ namespace PFMS.API.Tests.Controllers
             Assert.NotNull(okResult);
 
             Assert.IsType<GenericSuccessResponse<List<UserResponseModel>>>(okResult.Value);
+        }
+
+        [Fact]
+        public async void Get_User_By_Id_When_Details_Are_Returned_Test()
+        {
+            //Arrange
+            Mock<IUserService> userService = new Mock<IUserService>();
+            Mock<IMapper> mapper = new Mock<IMapper>();
+            UserController userController = new UserController(userService.Object, mapper.Object);
+
+            userService.Setup(x => x.GetUserProfile(It.IsAny<Guid>())).ReturnsAsync(new UserBo());
+            mapper.Setup(x => x.Map<UserResponseModel>(It.IsAny<UserBo>())).Returns(new UserResponseModel());
+
+            //Act
+            var response = await userController.GetById(Guid.NewGuid());
+
+            //Assert
+            Assert.NotNull(response);
+
+            var okResult = response as OkObjectResult;
+            Assert.NotNull(okResult);
+
+            Assert.IsType<GenericSuccessResponse<UserResponseModel>>(okResult.Value);
+        }
+
+        [Fact]
+        public async void Get_User_Profile_Returns_User_Details_Test()
+        {
+            //Arrange
+            Mock<IUserService> userService = new Mock<IUserService>();
+            Mock<IMapper> mapper = new Mock<IMapper>();
+            UserController userController = new UserController(userService.Object, mapper.Object);
+
+            userService.Setup(x => x.GetUserProfile(It.IsAny<Guid>())).ReturnsAsync(new UserBo());
+            mapper.Setup(x => x.Map<UserResponseModel>(It.IsAny<UserBo>())).Returns(new UserResponseModel());
+
+            //Act
+            var response = await userController.GetProfileAsync();
+
+            //Assert
+            Assert.NotNull(response);
+
+            var okResult = response as OkObjectResult;
+            Assert.NotNull(okResult);
+
+            Assert.IsType<GenericSuccessResponse<UserResponseModel>>(okResult.Value);
         }
     }
 }
