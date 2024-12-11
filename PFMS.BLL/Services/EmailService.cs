a5a0201afe75511075cfbd2d1eaca2b374506c14
gkshtz <gkshitiz21@gmail.com>
2024-12-11 23:20:34 +0530

    Added email service which sends the email

---
+++ PFMS.BLL/Services/EmailService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using PFMS.BLL.BOs;
+using PFMS.BLL.Interfaces;
+
+namespace PFMS.BLL.Services
+{
+    public class EmailService: IEmailService
+    {
+        private readonly SmtpSettingsBo _smtpSettings;
+        public EmailService(IOptions<SmtpSettingsBo> smtpSettings)
+        {
+            _smtpSettings = smtpSettings.Value;
+        }
+
+        public async Task<bool> SendEmail(string to, string subject, string body)
+        {
+            using var smtpClient = new SmtpClient(_smtpSettings.Server, _smtpSettings.Port)
+            {
+                Credentials = new NetworkCredential(_smtpSettings.Username, _smtpSettings.Password),
+                EnableSsl = true
+            };
+
+            var mailMessage = new MailMessage()
+            {
+                From = new MailAddress(_smtpSettings.SenderEmail, _smtpSettings.SenderName),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            };
+
+            mailMessage.To.Add(to);
+
+            try
+            {
+                await smtpClient.SendMailAsync(mailMessage);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
