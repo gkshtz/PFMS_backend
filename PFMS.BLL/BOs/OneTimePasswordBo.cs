083b361c598b906011dd2a6d0212438fcb240395
gkshtz <gkshitiz21@gmail.com>
2024-12-17 14:25:32 +0530

    added IsVerified field

--- PFMS.BLL/BOs/OneTimePasswordBo.cs
+++ PFMS.BLL/BOs/OneTimePasswordBo.cs
@@ -12,6 +12,7 @@ namespace PFMS.BLL.BOs
         public string Otp { get; set; }
         public DateTime Expires { get; set; }
         public Guid UserId { get; set; }
+        public bool IsVerified { get; set; }
         public Guid UniqueDeviceId { get; set; }
     }
 }
--- PFMS.DAL/DTOs/OneTimePasswordDto.cs
+++ PFMS.DAL/DTOs/OneTimePasswordDto.cs
@@ -12,6 +12,7 @@ namespace PFMS.DAL.DTOs
         public string Otp { get; set; }
         public DateTime Expires { get; set; }
         public Guid UserId { get; set; }
+        public bool IsVerified { get; set; }
         public Guid UniqueDeviceId { get; set; }
     }
 }
