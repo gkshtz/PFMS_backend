a5e1cf0e1aabc9bb0f17bc4b538ebf3af4e2d521
gkshtz <gkshitiz21@gmail.com>
2024-12-29 16:22:41 +0530

    Added functionality to send mail when the budget is exceeded.

--- PFMS.Utils/Constants/ApplicationConstsants.cs
+++ PFMS.Utils/Constants/ApplicationConstsants.cs
@@ -25,6 +25,8 @@ namespace PFMS.Utils.Constants
 
         public const string BudgetMailSubject = "Budget Alert";
 
+        public const string BudgetExceededMailSubject = "Your Budget Exceeded!";
+
         public static string GenerateBudgetSetEmailBody(string userName, decimal budgetAmount, decimal spentPercentage, Months month, int year)
         { 
             string body = $"<p>Hi {userName},</p>\r\n<div>You have set budget of RS {budgetAmount} for {char.ToUpper(month.ToString()[0]) + month.ToString().Substring(1).ToLower()}, {year}. You have already spend {spentPercentage}% of total set budget.</div>\r\n<div>Thanks</div>";
@@ -33,6 +35,13 @@ namespace PFMS.Utils.Constants
         public static string GenerateOtpEmailBody(string otp, string firstName, int validityInMinutes)
         {
             string body = $"<p>Hi {firstName},<p>\r\n<div>One Time Password for resetting your password is {otp}. OTP is valid for {validityInMinutes} minutes<div>\r\n<div>Thanks</div>";
+            return body;
+        }
+
+        public static string GenerateBudgetExceededMailBody(string userName, Months month, int year, decimal exceededAmount)
+        {
+            string body;
+            body = $"<p>Hi {userName}</p>\r\n  <div>Your expenditure for {char.ToUpper(month.ToString()[0]) + month.ToString().Substring(1).ToLower()}, {year} has exceeded your budget by {exceededAmount}.</div>\r\n<div>Thanks</div>";
             return body;
         }
     }
