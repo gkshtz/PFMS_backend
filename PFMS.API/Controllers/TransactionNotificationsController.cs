60d3548771510f21a562323f5b77069b35ee8c32
gkshtz <gkshitiz21@gmail.com>
2025-02-17 15:47:32 +0530

    update notification

--- PFMS.API/Controllers/TransactionNotificationsController.cs
+++ PFMS.API/Controllers/TransactionNotificationsController.cs
@@ -1,7 +1,9 @@
-﻿using System.Security.Cryptography.Xml;
+﻿using System.Net;
+using System.Security.Cryptography.Xml;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
 using PFMS.API.Models;
 using PFMS.BLL.BOs;
 using PFMS.BLL.Interfaces;
@@ -62,6 +64,22 @@ namespace PFMS.API.Controllers
             {
                 StatusCode = 200,
                 ResponseData = notificationModel,
+                ResponseMessage = ResponseMessage.Success.ToString()
+            };
+            return Ok(response);
+        }
+
+        [HttpPatch]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateAsync([FromBody] TransactionNotificationRequestModel notificationModel, [FromRoute] Guid id)
+        {
+            var notificationBo = _mapper.Map<TransactionNotificationBo>(notificationModel);
+            await _notificationsService.UpdateTransactionNotificationAsync(notificationBo, id, UserId);
+
+            var response = new GenericSuccessResponse<bool>()
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                ResponseData = true,
                 ResponseMessage = ResponseMessage.Success.ToString()
             };
             return Ok(response);
