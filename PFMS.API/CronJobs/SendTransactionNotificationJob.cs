6f0ec36637bef0cdd73e9d04eaa36a0d0e2bbed3
gkshtz <gkshitiz21@gmail.com>
2025-02-19 22:59:29 +0530

    WIP Send Transaction notifcation job

---
+++ PFMS.API/CronJobs/SendTransactionNotificationJob.cs
@@ -0,0 +1,29 @@
+﻿namespace PFMS.API.CronJobs
+{
+    public class SendTransactionNotificationJob: BackgroundService
+    {
+        private readonly TimeSpan _timeSpan;
+        public SendTransactionNotificationJob()
+        {
+            _timeSpan = new TimeSpan(8, 0, 0);
+        }
+        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            while(!cancellationToken.IsCancellationRequested)
+            {
+                DateTime now = DateTime.Now;
+                TimeSpan delay;
+                if(now > DateTime.Today.Add(_timeSpan))
+                {
+                    delay = DateTime.Today.Add(_timeSpan).AddDays(1) - now;
+                }
+                else
+                {
+                    delay = DateTime.Today.Add(_timeSpan) - now;
+                }
+
+                await Task.Delay(delay);
+            }
+        }
+    }
+}
